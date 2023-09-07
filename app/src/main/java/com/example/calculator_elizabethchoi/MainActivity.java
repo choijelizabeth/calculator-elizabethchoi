93359c4a8fac13eb4af72eab26079ac581c1543d
choijelizabeth <113711429+choijelizabeth@users.noreply.github.com>
2023-09-07 11:03:53 -0500

    9/7
    Started java code and added style xml for buttons

--- app/src/main/java/com/example/calculator_elizabethchoi/MainActivity.java
+++ app/src/main/java/com/example/calculator_elizabethchoi/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculator_elizabethchoi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /*
+    enterNum method to hold numbers for textView
+     */
+    public void enterNum0(View v){
+        Button num0 = findViewById(R.id.button0);
+        int num;
+
+        if(num0.getText().toString().equals("0")){
+            num = 0;
+        }
+
     }
 }
